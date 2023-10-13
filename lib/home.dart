d2cf631ae77920ec32d197accce02ae3b75f9f00
Brian Nguyen <Phihieu1507@gmail.com>
2023-10-13 21:27:53 +0700

    update :iconbutton_increase_volume

--- lib/home.dart
+++ lib/home.dart
@@ -6,27 +6,40 @@ class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
 }
-
+  double _volume = 0.0;
 class _HomePageState extends State<HomePage> {
   @override
+ 
   Widget build(BuildContext context) {
     return Scaffold(
       //APPBAR
       appBar: AppBar(
         title: const Text('Hi ! Welcome'),
         actions: <Widget>[
+          //ICON BUTTON POP UP SNACKBAR
           IconButton(
             icon: const Icon(Icons.thumb_up_alt),
-            tooltip: 'Like',
+            tooltip: 'Like snackbar',
             onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('This is a snackbar "LIKED"')));
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(const SnackBar(content: Text('LIKED')));
             },
           ),
+          //ICON BUTTON INCREASE VOLUME 
+          IconButton(
+            icon: const Icon(Icons.volume_up),
+            tooltip: 'Increase volume by 10',
+            onPressed: () {
+              setState(() {
+                _volume += 10;
+              });
+            },
+          ),
+          
         ],
       ),
       //BODY
-      body: const Text('THIS IS BODY'),
+      body: Text('THIS IS BODY SITE : Increase ->  Volume : $_volume'),
       //BOTTOM_NAV
       bottomNavigationBar: BottomNavigationBar(
         items: const [
