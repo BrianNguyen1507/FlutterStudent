b4ae99d656a39df7f5037896f150546252ea2c39
Brian Nguyen <Phihieu1507@gmail.com>
2023-10-13 15:43:14 +0700

    update: appbar_iconbutton

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -10,22 +11,43 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Hi ! Welcome'),),
-      body: const Text('THIS IS BODY'),
-      bottomNavigationBar: BottomNavigationBar(items: const [
-        BottomNavigationBarItem(
-          label: 'Home' ,
-          icon: Icon(Icons.home),
+      //APPBAR
+      appBar: AppBar(
+        title: const Text('Hi ! Welcome'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.thumb_up_alt),
+            tooltip: 'Like',
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('This is a snackbar "LIKED"')));
+            },
           ),
-       BottomNavigationBarItem(
-          label: 'Settings' ,
-          icon: Icon(Icons.settings),
+        ],
+      ),
+      //BODY
+      body: const Text('THIS IS BODY'),
+      //BOTTOM_NAV
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(
+              Icons.home,
+              semanticLabel: 'Text detail',
+              color: Colors.blue,
+            ),
           ),
           BottomNavigationBarItem(
-          label: 'Search' ,
-          icon: Icon(Icons.search),
+            label: 'Search',
+            icon: Icon(Icons.search),
           ),
-      ],),
+          BottomNavigationBarItem(
+            label: 'Settings',
+            icon: Icon(Icons.settings),
+          ),
+        ],
+      ),
     );
   }
 }
